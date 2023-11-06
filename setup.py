744f1ede48993d8ca1fbde8033f0b0d0091adc9b
Ishan Oshada <63636533+Ishanoshada@users.noreply.github.com>
2023-11-07 04:22:12 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='sadaa',
-    version='0.2.0',
+    version='0.3.0',
     packages=find_packages(),
     classifiers=[
         'Programming Language :: Python :: 3',
