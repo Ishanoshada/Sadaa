b3c16fbfaf3e1b0484b9e1647bc5fc4d65f9bc6a
Ishan Oshada <63636533+Ishanoshada@users.noreply.github.com>
2023-11-07 01:14:35 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='sadaa',
+    version='0.2.0',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    author='Ishan Oshada',
+    author_email='ishan.kodithuwakku.com',
+    description='A package for generating random stories.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/ishanoshada/sadaa',
+    project_urls={
+        'Source': 'https://github.com/ishanoshada/sadaa',
+    },
+)
